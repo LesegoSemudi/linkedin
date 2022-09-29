2b136188ac17a21bc8848d8a72aa523b7379d8f9
Lesego Semudi <94657451+LesegoSemudi@users.noreply.github.com>
2022-09-29 03:52:50 +0200

    linkedin-clone

---
+++ linkedin-clone/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from "./header";
+import './App.css';
+
+function App() {
+    return ( <div className = "app" >
+        <h1> Lets Build a linkedin clone </h1>
+
+        <header>
+            
+        </header>
+
+        </div>
+    );
+}
+
+export default App;
---
+++ linkedin-clone/src/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './header.css'
+
+function header() {
+    return ( <div className = 'header' >
+        <h1> This is the header </h1> 
+        </div >
+    )
+}
+
+export default header
